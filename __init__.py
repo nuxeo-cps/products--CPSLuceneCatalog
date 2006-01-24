8a6db6d4e0f746e0ba8fa48afc1bc6f22c7cb8a5
janguenot <devnull@localhost>
2006-01-24 14:13:39 +0000

    Here we go

---
+++ __init__.py
@@ -0,0 +1,31 @@
+# (C) Copyright 2006 Nuxeo SAS <http://nuxeo.com>
+# Author: Julien Anguenot <ja@nuxeo.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+# $Id$
+"""CPS Lucene Catalog
+"""
+
+from Products.CMFCore.utils import ToolInit
+
+import catalog
+
+def initialize(registrar):
+    ToolInit(
+        meta_type="CPS Lucene Catalog",
+        tools=(catalog.CPSLuceneCatalog,),
+        icon="tool.png",
+        ).initialize(registrar)
